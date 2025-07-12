9a677ffd52b3aea2ad76906fa87eed7a92a79725
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-07-12 17:20:23 -0600

    blog class

---
+++ Python/OOP/Blog.py
@@ -0,0 +1,23 @@
+"""This script contains the Blog class for managing blog posts."""
+
+class Blog:
+    """A class to represent a blog with posts."""
+
+    def __init__(self):
+        pass
+
+    def add_post(self, post:object) -> None:
+        """Adds a post to the blog.
+        
+        Args:
+            post (object): The post object to add to the blog.
+        """
+        pass
+
+    def get_posts(self) -> list:
+        """Retrieves all posts from the blog.
+        
+        Returns:
+            list: A list of all post objects in the blog.
+        """
+        pass
