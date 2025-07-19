83101cb65270d2187c91c66ccba52030f24c5d52
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-07-18 20:23:42 -0600

    conditional if

--- CS/test1/Program.cs
+++ CS/test1/Program.cs
@@ -13,3 +13,14 @@ string inputAge = Console.ReadLine();
 int age = int.Parse(inputAge);
 
 Console.WriteLine($"Hello {name}, you are {age} years old.");
+
+// Conditional statements if
+if (age < 18)
+{
+    Console.WriteLine("You are a minor.");
+    Console.WriteLine($"To be an adult you need {18 - age} more years");
+}
+else
+{
+    Console.WriteLine("You are an adult.");
+}
