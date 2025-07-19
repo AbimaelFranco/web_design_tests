c9402987fbab9f827e3dbf79e6e706465914b7fb
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-07-18 20:22:35 -0600

    Console input-output

--- CS/test1/Program.cs
+++ CS/test1/Program.cs
@@ -3,3 +3,13 @@ int userAge = 0;
 string userName = "Alex";
 bool state = true;
 
+// Console input and output
+Console.WriteLine("Welcome to the program!");
+Console.Write("Enter your name: ");
+string name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+string inputAge = Console.ReadLine();
+int age = int.Parse(inputAge);
+
+Console.WriteLine($"Hello {name}, you are {age} years old.");
